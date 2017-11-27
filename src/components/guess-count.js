3d6f65159cf6be13204d6ba6ab0dbcb7f0f5f977
Joe Turner <joe@oampo.co.uk>
2017-11-27 14:24:57 +0000

    Replace &apos

--- src/components/guess-count.js
+++ src/components/guess-count.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 
 import './guess-count.css';
 
 export function GuessCount(props) {
-  const isPlural = props.guessCount !== 1;
-  const guessNoun = isPlural ? 'guesses' : 'guess';
+    const isPlural = props.guessCount !== 1;
+    const guessNoun = isPlural ? 'guesses' : 'guess';
 
-  return (
-    <h2 id="guessCount">
-      You&apos;ve made <span id="count">{props.guessCount}</span> {guessNoun}!
-    </h2>
-  );
+    return (
+        <h2 id="guessCount">
+            You've made <span id="count">{props.guessCount}</span> {guessNoun}!
+        </h2>
+    );
 }
 
 const mapStateToProps = state => ({
-  guessCount: state.guesses.length
+    guessCount: state.guesses.length
 });
 
 export default connect(mapStateToProps)(GuessCount);
