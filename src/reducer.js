8eb25649becac40f07d05780a6567664c9226c43
Joe Turner <joe@oampo.co.uk>
2017-11-28 12:27:26 +0000

    Remove document.title setting
    Might tackle later, but this should be a component, and definitely not
    the reducer

--- src/reducer.js
+++ src/reducer.js
@@ -1,78 +1,74 @@
-import { RESTART_GAME, MAKE_GUESS, GENERATE_AURAL_UPDATE } from './actions';
+import {RESTART_GAME, MAKE_GUESS, GENERATE_AURAL_UPDATE} from './actions';
 
 const initialState = {
-  guesses: [],
-  feedback: 'Make your guess!',
-  auralStatus: '',
-  correctAnswer: Math.round(Math.random() * 100) + 1
+    guesses: [],
+    feedback: 'Make your guess!',
+    auralStatus: '',
+    correctAnswer: Math.round(Math.random() * 100) + 1
 };
 
-export default (state=initialState, action) => {
-
-  if (action.type === RESTART_GAME) {
-    
-    return Object.assign({}, state, {
-      guesses: [],
-      feedback: 'Make your guess!',
-      auralStatus: '',
-      correctAnswer: Math.round(Math.random() * 100) + 1
-    });
-  }
-
-  if (action.type === MAKE_GUESS) {
-    let feedback, guess;
-
-    guess = parseInt(action.guess, 10);
-    if (isNaN(guess)) {
-      feedback = 'Please enter a valid number.';
-
-      return Object.assign({}, state, {
-        feedback,
-        guesses: [...state.guesses, guess]
-      }); 
+export default (state = initialState, action) => {
+    if (action.type === RESTART_GAME) {
+        return Object.assign({}, state, {
+            guesses: [],
+            feedback: 'Make your guess!',
+            auralStatus: '',
+            correctAnswer: Math.round(Math.random() * 100) + 1
+        });
     }
 
-    const difference = Math.abs(guess - state.correctAnswer);
+    if (action.type === MAKE_GUESS) {
+        let feedback, guess;
 
-    if (difference >= 50) {
-      feedback = 'You\'re Ice Cold...';
-    } else if (difference >= 30) {
-      feedback = 'You\'re Cold...';
-    } else if (difference >= 10) {
-      feedback = 'You\'re Warm.';
-    } else if (difference >= 1) {
-      feedback = 'You\'re Hot!';
-    } else {
-      feedback = 'You got it!';
+        guess = parseInt(action.guess, 10);
+        if (isNaN(guess)) {
+            feedback = 'Please enter a valid number.';
+
+            return Object.assign({}, state, {
+                feedback,
+                guesses: [...state.guesses, guess]
+            });
+        }
+
+        const difference = Math.abs(guess - state.correctAnswer);
+
+        if (difference >= 50) {
+            feedback = "You're Ice Cold...";
+        } else if (difference >= 30) {
+            feedback = "You're Cold...";
+        } else if (difference >= 10) {
+            feedback = "You're Warm.";
+        } else if (difference >= 1) {
+            feedback = "You're Hot!";
+        } else {
+            feedback = 'You got it!';
+        }
+
+        return Object.assign({}, state, {
+            feedback,
+            guesses: [...state.guesses, guess]
+        });
     }
 
-    // We typically wouldn't touch the DOM directly like this in React
-    // but this is the best way to update the title of the page,
-    // which is good for giving screen-reader users
-    // instant information about the app.
-    document.title = feedback ? `${feedback} | Hot or Cold` : 'Hot or Cold';
+    if (action.type === GENERATE_AURAL_UPDATE) {
+        const {guesses, feedback} = state;
 
-    return Object.assign({}, state, {
-      feedback,
-      guesses: [...state.guesses, guess]
-    });
-  }
+        // If there's not exactly 1 guess, we want to
+        // pluralize the nouns in this aural update.
+        const pluralize = guesses.length !== 1;
 
-  if (action.type === GENERATE_AURAL_UPDATE) {
-    const { guesses, feedback } = state;
-    
-    // If there's not exactly 1 guess, we want to
-    // pluralize the nouns in this aural update.
-    const pluralize = guesses.length !== 1;
+        let auralStatus = `Here's the status of the game right now: ${feedback} You've made ${guesses.length} ${pluralize
+            ? 'guesses'
+            : 'guess'}.`;
 
-    let  auralStatus = `Here's the status of the game right now: ${feedback} You've made ${guesses.length} ${pluralize ? 'guesses' : 'guess'}.`;
+        if (guesses.length > 0) {
+            auralStatus += ` ${pluralize
+                ? 'In order of most- to least-recent, they are'
+                : 'It was'}: ${guesses.reverse().join(', ')}`;
+        }
 
-    if (guesses.length > 0) {
-      auralStatus += ` ${pluralize ? 'In order of most- to least-recent, they are' : 'It was'}: ${guesses.reverse().join(', ')}`;
+        return Object.assign({}, state, {auralStatus});
     }
 
-    return Object.assign({}, state, { auralStatus });
-  }
-
-  return state;
+    return state;
 };
